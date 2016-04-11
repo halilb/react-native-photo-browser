0688401a9b49ff488922e8eff354127c7f8aaa01
Halil Bilir <hbilir@gmail.com>
2016-04-11 14:03:27 +0300

    initial ListView implementation

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -7,16 +7,34 @@ import React, {
   AppRegistry,
   Component,
   StyleSheet,
+  ListView,
   View,
 } from 'react-native';
 
 import PhotoBrowser from 'react-native-photo-browser';
 
 class PhotoBrowserExample extends Component {
+
+  constructor(props) {
+    super(props);
+
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
+    this.state = {
+      dataSource: dataSource.cloneWithRows([
+        'http://www.cybermagonline.com/wp-content/uploads/2016/02/google-logo.jpg',
+        'https://octodex.github.com/images/codercat.jpg',
+      ]),
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <PhotoBrowser />
+        <PhotoBrowser
+          dataSource={this.state.dataSource}
+        />
       </View>
     );
   }
@@ -25,9 +43,6 @@ class PhotoBrowserExample extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
 });
 
--- PhotoBrowser.js
+++ PhotoBrowser.js
@@ -1,16 +1,65 @@
 import React, {
-  Text,
+  Dimensions,
+  Image,
+  ListView,
   View,
+  StyleSheet,
+  PropTypes,
 } from 'react-native';
+
+const sizes = Dimensions.get('window');
+const screenWidth = sizes.width;
+const screenHeight = sizes.height;
 
 export default class PhotoBrowser extends React.Component {
 
+  static propTypes = {
+    dataSource: PropTypes.instanceOf(ListView.DataSource).isRequired,
+  };
+
+  constructor(props, context) {
+    super(props, context);
+
+    this._renderImage = this._renderImage.bind(this);
+  }
+
+  _renderImage(image) {
+    return (
+      <Image
+        style={styles.image}
+        source={{ uri: image }}
+        resizeMode="contain"
+      />
+    );
+  }
+
   render() {
     return (
-      <View>
-        <Text>Initial</Text>
+      <View style={styles.container}>
+        <ListView
+          style={styles.list}
+          dataSource={this.props.dataSource}
+          renderRow={this._renderImage}
+          horizontal
+          pagingEnabled
+          maximumZoomScale={5.0}
+          showsHorizontalScrollIndicator={false}
+        />
       </View>
     );
   }
 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'black',
+  },
+  list: {
+    flex: 1,
+  },
+  image: {
+    width: screenWidth,
+    height: screenHeight,
+  },
+});
