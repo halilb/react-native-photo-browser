56c589dbb94d37becbcea377bffd75821cd15e71
Halil Bilir <hbilir@gmail.com>
2016-04-22 16:34:32 +0300

    displaying media captions

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -18,17 +18,20 @@ class PhotoBrowserExample extends Component {
   constructor(props) {
     super(props);
 
-    const dataSource = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2,
-    });
+    const media = [{
+      thumb: 'http://farm3.static.flickr.com/2667/4072710001_f36316ddc7_q.jpg?',
+      photo: 'http://farm3.static.flickr.com/2667/4072710001_f36316ddc7_b.jpg?',
+      selected: true,
+      caption: 'Grotto of the Madonna',
+    }, {
+      thumb: 'http://farm3.static.flickr.com/2449/4052876281_6e068ac860_q.jpg',
+      photo: 'http://farm3.static.flickr.com/2449/4052876281_6e068ac860_b.jpg',
+      selected: false,
+      caption: 'Beautiful Eyes',
+    }];
+
     this.state = {
-      dataSource: dataSource.cloneWithRows([
-        'http://farm4.static.flickr.com/3567/3523321514_371d9ac42f_b.jpg',
-        'http://farm4.static.flickr.com/3629/3339128908_7aecabc34b_b.jpg',
-        'https://farm6.staticflickr.com/5521/10852449723_a5b8a4620b_o_d.jpg',
-        'http://farm4.static.flickr.com/3364/3338617424_7ff836d55f_b.jpg',
-        'http://farm4.static.flickr.com/3590/3329114220_5fbc5bc92b_b.jpg',
-      ]),
+      media,
     };
   }
 
@@ -36,8 +39,8 @@ class PhotoBrowserExample extends Component {
     return (
       <View style={styles.container}>
         <PhotoBrowser
-          dataSource={this.state.dataSource}
-          initialIndex={2}
+          media={this.state.media}
+          initialIndex={0}
           displayNavArrows
           displaySelectionButtons
           startOnGrid
