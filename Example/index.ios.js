d2adf0e28f9124c5556197161fbf2cd76c37b78d
Halil Bilir <hbilir@gmail.com>
2016-04-29 14:54:03 +0300

    created three different examples

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -7,11 +7,60 @@ import React, {
   AppRegistry,
   CameraRoll,
   Component,
+  ListView,
   StyleSheet,
+  Navigator,
+  Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
 import PhotoBrowser from 'react-native-photo-browser';
+
+const EXAMPLES = [
+  {
+    title: 'Single photo',
+    description: 'with caption, no grid button',
+    enableGrid: false,
+    media: [{
+      photo: 'http://farm3.static.flickr.com/2667/4072710001_f36316ddc7_b.jpg',
+      caption: 'Grotto of the Madonna',
+    }],
+  }, {
+    title: 'Multiple photos',
+    description: 'with captions and nav arrows',
+    displayNavArrows: true,
+    media: [{
+      photo: 'http://farm3.static.flickr.com/2667/4072710001_f36316ddc7_b.jpg',
+      selected: true,
+      caption: 'Grotto of the Madonna',
+    }, {
+      photo: 'http://farm3.static.flickr.com/2449/4052876281_6e068ac860_b.jpg',
+      thumb: 'http://farm3.static.flickr.com/2449/4052876281_6e068ac860_q.jpg',
+      selected: false,
+      caption: 'Beautiful Eyes',
+    }],
+  }, {
+    title: 'Library photos',
+    description: 'showing grid first, custom action method',
+    startOnGrid: true,
+    onAction: (media, index) => {
+      console.log(`action button pressed for ${media.photo}, index: ${index}`);
+    },
+  },
+];
+
+// fill 'Library photos' example with local media
+CameraRoll.getPhotos({
+  first: 30,
+  assetType: 'Photos',
+}).then((data) => {
+  const media = [];
+  data.edges.forEach(d => media.push({
+    photo: d.node.image.uri,
+  }));
+  EXAMPLES[2].media = media;
+}).catch(error => alert(error));
 
 class PhotoBrowserExample extends Component {
 
@@ -19,65 +68,112 @@ class PhotoBrowserExample extends Component {
     super(props);
 
     this._onSelectionChanged = this._onSelectionChanged.bind(this);
-    this._onActionButtonPressed = this._onActionButtonPressed.bind(this);
+    this._renderRow = this._renderRow.bind(this);
+    this._renderScene = this._renderScene.bind(this);
 
-    const media = [{
-      photo: 'http://farm3.static.flickr.com/2667/4072710001_f36316ddc7_b.jpg?',
-      selected: true,
-      caption: 'Grotto of the Madonna',
-    }, {
-      photo: 'http://farm3.static.flickr.com/2449/4052876281_6e068ac860_b.jpg',
-      selected: false,
-      caption: 'Beautiful Eyes',
-    }];
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
 
     this.state = {
-      media: [],
+      dataSource: dataSource.cloneWithRows(EXAMPLES),
     };
-  }
-
-  componentDidMount() {
-    CameraRoll.getPhotos({
-      first: 100,
-      assetType: 'Photos',
-    }).then((data) => {
-      const media = [];
-      data.edges.forEach(d => media.push({
-        photo: d.node.image.uri,
-      }));
-
-      this.setState({ media });
-    }).catch(error => alert(error));
   }
 
   _onSelectionChanged(media, index, selected) {
     console.log(`${media.photo} selection status: ${selected}`);
   }
 
-  _onActionButtonPressed(media, index) {
-    console.log(`action button pressed for ${media.photo}, index: ${index}`);
+  _openExample(example) {
+    this.refs.nav.push(example);
+  }
+
+  _renderRow(rowData, sectionID, rowID) {
+    const example = EXAMPLES[rowID];
+
+    return (
+      <TouchableOpacity onPress={() => this._openExample(example) }>
+        <View style={styles.row}>
+          <Text style={styles.rowTitle}>{rowData.title}</Text>
+          <Text style={styles.rowDescription}>{rowData.description}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  _renderScene(route, navigator) {
+    if (route.index === 0) {
+      return (
+        <View style={styles.container}>
+          <ListView
+            style={styles.list}
+            dataSource={this.state.dataSource}
+            renderRow={this._renderRow}
+          />
+        </View>
+      );
+    }
+
+    const {
+      media,
+      initialIndex,
+      displayNavArrows,
+      displayActionButton,
+      displaySelectionButtons,
+      startOnGrid,
+      enableGrid,
+      onAction,
+    } = route;
+
+    return (
+      <PhotoBrowser
+        onBack={navigator.pop}
+        mediaList={media}
+        initialIndex={initialIndex}
+        displayNavArrows={displayNavArrows}
+        displaySelectionButtons={displaySelectionButtons}
+        displayActionButton={displayActionButton}
+        startOnGrid={startOnGrid}
+        enableGrid={enableGrid}
+        useCircleProgress
+        onSelectionChanged={this._onSelectionChanged}
+        onActionButton={onAction}
+      />
+    );
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <PhotoBrowser
-          mediaList={this.state.media}
-          initialIndex={1}
-          displayNavArrows
-          displaySelectionButtons
-          onSelectionChanged={this._onSelectionChanged}
-          startOnGrid
-          useCircleProgress
-        />
-      </View>
+      <Navigator
+        ref="nav"
+        initialRoute={{ index: 0 }}
+        renderScene={this._renderScene}
+      />
     );
   }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  list: {
+    flex: 1,
+    paddingTop: 54,
+    paddingLeft: 16,
+  },
+  row: {
+    flex: 1,
+    padding: 8,
+    borderBottomColor: 'rgba(0, 0, 0, 0.1)',
+    borderBottomWidth: 1,
+  },
+  rowTitle: {
+    fontSize: 14,
+  },
+  rowDescription: {
+    fontSize: 12,
   },
 });
 
